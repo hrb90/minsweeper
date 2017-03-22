2e6c6e84b752362adbcf6de05c2142c1c82bbdd0
hrb90 <harrisonrbrown90@gmail.com>
2017-03-22 09:25:16 -0400

    Rebundle, add gitignore

--- bundle.js
+++ bundle.js
@@ -90,7 +90,7 @@ class Board {
   }
 
   revealBombs () {
-    this._flatGrid.forEach((tile) => {
+    this._flatGrid().forEach((tile) => {
       if (tile.isBomb()) tile.reveal();
     });
   }
@@ -426,6 +426,10 @@ module.exports = Board;
 	      });
 	    });
 	  }
+
+		toArray () {
+			return this.els;
+		}
 	}
 
 	function arrayDifference(arr1, arr2) {
@@ -512,26 +516,54 @@ module.exports = Tile;
 const Board = __webpack_require__(0);
 __webpack_require__(1);
 
-const render = (boardEl, board) => {
-  boardEl.remove(".tile");
+const makeTile = (rowIdx, colIdx) => {
+  let tileEl = document.createElement("div");
+  tileEl.classList.add("tile");
+  tileEl.setAttribute("data-row", rowIdx);
+  tileEl.setAttribute("data-col", colIdx);
+  return tileEl;
+};
+
+const populateBoard = boardEl => {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j ++) {
+      let tile = makeTile(i, j);
+      boardEl.append(tile);
+    }
+  }
+};
+
+const virtualBoard = boardEl => {
+  let vBoard = [];
+  let tiles = boardEl.children().toArray();
+  for (let i = 0; i < 16; i++) {
+    let row = [];
+    for (let j = 0; j < 16; j ++) {
+      row.push(tiles[16 * i + j]);
+    }
+    vBoard.push(row);
+  }
+  return vBoard;
+};
+
+const render = (vBoard, board) => {
   board.publicGrid().forEach((row, rowIdx) => {
     row.forEach((tile, colIdx) => {
-      let tileEl = document.createElement("div");
-      tileEl.classList.add("tile");
-      tileEl.setAttribute("data-row", rowIdx);
-      tileEl.setAttribute("data-col", colIdx);
-      tileEl.innerHTML = tile;
-      boardEl.append(tileEl);
+      vBoard[rowIdx][colIdx].className = `tile ${tile}`;
+      vBoard[rowIdx][colIdx].innerHTML = tile;
     });
   });
+};
+
+const addListeners = (vBoard, board) => {
   $l(".tile").on("click", e => {
     board.reveal([+(e.target.dataset.row), +(e.target.dataset.col)]);
-    render(boardEl, board);
+    render(vBoard, board);
   });
   $l(".tile").on("contextmenu", e => {
     e.preventDefault();
     board.flag([+(e.target.dataset.row), +(e.target.dataset.col)]);
-    render(boardEl, board);
+    render(vBoard, board);
     return false;
   });
 };
@@ -539,7 +571,10 @@ const render = (boardEl, board) => {
 $l(function() {
   let board = new Board(16, 16, 28);
   let boardEl = $l("#board");
-  render(boardEl, board);
+  populateBoard(boardEl);
+  let vBoard = virtualBoard(boardEl);
+  addListeners(vBoard, board);
+  render(vBoard, board);
 });
 
 
